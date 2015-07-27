09517dc68a4c50cfcaf7be9ff20b88331c1a470a
BalajiAmarnath <balajigunyacloud@gmail.com>
2015-07-27 10:52:04 +0530

    Added DisplayState function

--- TennisScoring.java
+++ TennisScoring.java
@@ -14,10 +14,27 @@ class TennisScoring {
             else
                 state_f+=1;
 	}
-	static void displayState()
+	
+	public void displayState()
 	{
-		//Displays the present score to be displayed 
+		if(gameOver()){
+			state_d = 0;
+			state_e =0;
+		}
+		if (state_d == 3 && state_e == 3){
+			System.out.println("deuce");
+		}
+		else if (state_d == 4 && state_e == 3) {
+			System.out.println("Advantage D");
+		}
+		else if (state_d == 3 && state_e == 4) {
+			System.out.println("Advantage F");
+		}
+		else {
+			System.out.println( "D : " + scoreMap[state_d] + "E : " + scoreMap[state_e])
+		}
 	}
+	
 	static void nextState(char point)
 	{
 		//computes the next point score
