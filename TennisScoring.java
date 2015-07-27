2871f87455ebec5284771707b7ae6aa2ef80559c
RamprasadMurugesan <m.ramprasad.cbe@gmail.com>
2015-07-27 10:50:59 +0530

    added nextState function

--- TennisScoring.java
+++ TennisScoring.java
@@ -2,9 +2,17 @@ class TennisScoring {
 	private int state_d,state_f;
 	private int[] scoreMap = {0, 15, 30, 40};
 	
-	static void computeScore(String input)
+	static void nextState(char point)
 	{
-		//Computing the score from the given string
+		if((state_d==4)&&(state_f==4))
+            {
+                state_d-=1;
+                state_f-=1;
+            }
+            if(point=='d')
+                state_d+=1;
+            else
+                state_f+=1;
 	}
 	static void displayState()
 	{
