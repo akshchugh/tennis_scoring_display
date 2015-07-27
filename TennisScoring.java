8d4a6bc7f4ecde9b4c157a1f4d8f53e19d5fc73f
RamprasadMurugesan <m.ramprasad.cbe@gmail.com>
2015-07-27 11:22:10 +0530

    final code changes integrated

--- TennisScoring.java
+++ TennisScoring.java
@@ -1,66 +1,79 @@
-class TennisScoring {
-	private int state_d,state_f;
-	private int[] scoreMap = {0, 15, 30, 40};
-	
-	public static void main(String[] args) {
-        // TODO code application logic here
-        
-        String input="dddd";
-        TennisScoring s=new TennisScoring();
-        
-        s.displayState();
-        for(int i=0;i<input.length();i++)
-        {
-        s.nextState(input.charAt(i));
-        s.displayState();
-        }
-	
-	public void nextState(char point)
-	{
-		if((state_d==4)&&(state_f==4))
-            {
-                state_d-=1;
-                state_f-=1;
-            }
+package tennisscoring;
+
+import java.util.Scanner;
+
+
+public class TennisScoring {
+
+    private int state_d=0;
+    private int state_f=0;
+    private int[] scoreMap = {0, 15, 30, 40};
+    
+   
+    
+    
+     void nextState(char point)
+    {
             if(point=='d')
                 state_d+=1;
             else
                 state_f+=1;
-	}
-	
-	public void displayState()
-	{
-		if(gameOver()){
+            
+            if((state_d==4)&&(state_f==4))
+            {
+                state_d-=1;
+                state_f-=1;
+                
+            }
+    }
+     
+    public boolean gameOver()
+    {
+        if ( state_f < 4 && state_d < 4) {
+            return false;
+        }
+        if ( state_f - state_d >= 2) {
+            System.out.print ("F won the game ; ");
+            return true;
+        }
+        else if ( state_d - state_f >=2) {
+            System.out.print ("D won the game ; ");
+            return true;
+        }
+        return false;
+    }
+    
+    void displayState()
+    {
+        
+        if(gameOver()){
 			state_d = 0;
 			state_f =0;
 		}
-		else if (state_d == 4 && state_f == 3) {
+		
+                else if (state_d == 4 && state_f == 3) {
 			System.out.println("Advantage D");
 		}
 		else if (state_d == 3 && state_f == 4) {
 			System.out.println("Advantage F");
 		}
 		else {
-			System.out.println( "D : " + scoreMap[state_d] + "\t F : " + scoreMap[state_f]);
+			System.out.println( "D : " + scoreMap[state_d] + "  F : " + scoreMap[state_f]);
 		}
-	}
-	
-	
-	static boolean gameOver()
-	{
-		if ( state_f < 4 && state_d < 4) {
-			return false;
-		}
-		if ( state_f - state_d >= 2) {
-			System.out.print ("F won the game ");
-			return true;
-		}
-		else if ( state_d - state_f >=2) {
-			System.out.print ("D won the game ");
-			return true;
-		}
-		return false;
-	}
-	
-	
+        
+    }
+    public static void main(String[] args) {
+        
+        Scanner scanner=new Scanner(System.in);
+        String input=scanner.next();
+        TennisScoring score=new TennisScoring();
+        score.displayState();
+        for(int i=0;i<input.length();i++)
+        {
+            score.nextState(input.charAt(i));
+            score.displayState();
+        }       
+        score.displayState();
+    }
+    
 }
