3d0972833133abce989ff745c404d61e425da2de
BalajiAmarnath <balajigunyacloud@gmail.com>
2015-07-27 10:49:17 +0530

    added the member variables

--- TennisScoring.java
+++ TennisScoring.java
@@ -1,4 +1,7 @@
 class TennisScoring {
+	private int state_d,state_f;
+	private int[] scoreMap = {0, 15, 30, 40};
+	
 	static void computeScore(String input)
 	{
 		//Computing the score from the given string
