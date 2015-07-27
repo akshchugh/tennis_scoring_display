7a91f44d5a318a04f5fee60406807f0e949de845
Aakash Chugh <akshchugh.ac@gmail.com>
2015-07-27 10:38:44 +0530

    Specified the required API's

---
+++ TennisScoring.java
@@ -0,0 +1,14 @@
+class TennisScoring {
+	static void computeScore(String input)
+	{
+		//Computing the score from the given string
+	}
+	static void displayState()
+	{
+		//Displays the present score to be displayed 
+	}
+	static void nextState(char point)
+	{
+		//computes the next point score
+	}
+}
