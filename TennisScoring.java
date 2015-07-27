d418aa2a7890c27bdddb5982eb5740cd04faa98d
RamprasadMurugesan <m.ramprasad.cbe@gmail.com>
2015-07-27 11:35:59 +0530

    improved code alignment

--- TennisScoring.java
+++ TennisScoring.java
@@ -2,33 +2,25 @@ package tennisscoring;
 
 import java.util.Scanner;
 
-
 public class TennisScoring {
 
     private int state_d=0;
     private int state_f=0;
     private int[] scoreMap = {0, 15, 30, 40};
-    
-   
-    
-    
-     void nextState(char point)
-    {
-            if(point=='d')
+        
+     void nextState(char point) {
+            if (point=='d')
                 state_d+=1;
             else
                 state_f+=1;
             
-            if((state_d==4)&&(state_f==4))
-            {
+            if ((state_d==4)&&(state_f==4)) {
                 state_d-=1;
                 state_f-=1;
-                
             }
     }
      
-    public boolean gameOver()
-    {
+    public boolean gameOver() {
         if ( state_f < 4 && state_d < 4) {
             return false;
         }
@@ -43,23 +35,20 @@ public class TennisScoring {
         return false;
     }
     
-    void displayState()
-    {
-        
+    void displayState() {        
         if(gameOver()){
-			state_d = 0;
-			state_f =0;
-		}
-		
-                else if (state_d == 4 && state_f == 3) {
-			System.out.println("Advantage D");
-		}
-		else if (state_d == 3 && state_f == 4) {
-			System.out.println("Advantage F");
-		}
-		else {
-			System.out.println( "D : " + scoreMap[state_d] + "  F : " + scoreMap[state_f]);
-		}
+            state_d = 0;
+            state_f =0;
+        }
+        else if (state_d == 4 && state_f == 3) {
+            System.out.println("Advantage D");
+        }
+        else if (state_d == 3 && state_f == 4) {
+            System.out.println("Advantage F");
+        }
+        else {
+            System.out.println( "D : " + scoreMap[state_d] + "  F : " + scoreMap[state_f]);
+        }
         
     }
     public static void main(String[] args) {
@@ -73,7 +62,7 @@ public class TennisScoring {
             score.nextState(input.charAt(i));
             score.displayState();
         }       
-        
+       
     }
     
 }
